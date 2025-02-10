37a0cab0eabcc0f28676494e8d14ef2355f41094
Dhanvanth Sathyamangalam Balakrishnan <dhanvanth.balakrishnan@int.trustrace.com>
2025-02-10 18:04:00 +0530

    feb 10 works

--- src/main/java/com/trustrace/survey_time/model/Response.java
+++ src/main/java/com/trustrace/survey_time/model/Response.java
@@ -4,6 +4,7 @@ package com.trustrace.survey_time.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.Map;
@@ -13,7 +14,14 @@ import java.util.Map;
 @AllArgsConstructor
 @Document(collection = "responses")
 public class Response {
+
+
+    @Id
     private String id;
+
+    private String name;
+    private String email;
+    private String surveyId;
 
     private Map<String,Object> responseObject;
 
@@ -31,5 +39,29 @@ public class Response {
 
     public void setResponseObject(Map<String, Object> responseObject) {
         this.responseObject = responseObject;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSurveyId() {
+        return surveyId;
+    }
+
+    public void setSurveyId(String surveyId) {
+        this.surveyId = surveyId;
     }
 }
--- src/main/java/com/trustrace/survey_time/service/ResponseService.java
+++ src/main/java/com/trustrace/survey_time/service/ResponseService.java
@@ -17,23 +17,28 @@ public class ResponseService {
     private ResponseRepository responseRepository;
 
     // Save survey (POST request)
-    public Response saveSurvey(Response response) {
-        System.out.println("Survey object: " + response.getResponseObject());
+    public Response saveResponse(Response response) {
+        System.out.println("Response object: " + response.getResponseObject());
         return responseRepository.save(response);
     }
 
     // get survey list (GET request)
-    public List<Response> getAllSurveys() {
+    public List<Response> getAllResponses() {
         return responseRepository.findAll();
     }
 
     //get survey by id (GET request)
-    public Response getSurveyById(String id) {
+    public Response getReponseById(String id) {
         return responseRepository.findById(id).orElse(null);
     }
 
+    //get all response by email id
+    public List<Response> getAllResponseByEmailId(String email) {
+        return null;
+    }
+
     //delete survey by id (DELETE request)
-    public void deleteSurvey(String id) {
+    public void deleteResponse(String id) {
         responseRepository.deleteById(id);
     }
 }
